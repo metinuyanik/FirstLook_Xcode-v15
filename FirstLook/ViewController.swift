74b3e5cffc74de39c42ee0010c60a11c94e3f7dc
Metin Uyanik <metinuynk218@gmail.com>
2023-09-21 21:16:52 +0300

    Initial Commit

---
+++ FirstLook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstLook
+//
+//  Created by metin uyanık on 21.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
